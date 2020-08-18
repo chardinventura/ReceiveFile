89b216493ce4fd49e0b6245d6e01810c11dbb1c5
Chardin <chardindelacruzventura@gmail.com>
2020-08-18 07:10:06 +0000

    Actualizando import, agregando la libreria File

--- src/receivefile/Main.java
+++ src/receivefile/Main.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 
 import javax.swing.JFileChooser;
 
+import file.File;
+
 public class Main {
 
 	public static void main(String[] args) throws ClassNotFoundException, IOException, InterruptedException {
--- src/receivefile/ServerSocket.java
+++ src/receivefile/ServerSocket.java
@@ -5,6 +5,8 @@ import java.io.ObjectInputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 
+import file.File;
+
 public class ServerSocket extends java.net.ServerSocket{
 
 	private Socket socket;
