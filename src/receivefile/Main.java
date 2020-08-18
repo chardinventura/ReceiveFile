f838f3d6520aac1395214348d6cced53fefde891
Chardin <chardindelacruzventura@gmail.com>
2020-08-18 06:54:19 +0000

    Subiendo proyecto ReceiveFile

---
+++ src/receivefile/Main.java
@@ -0,0 +1,41 @@
+package receivefile;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.swing.JFileChooser;
+
+public class Main {
+
+	public static void main(String[] args) throws ClassNotFoundException, IOException, InterruptedException {
+		
+		ServerSocket serverSocket = new ServerSocket(7788);
+		
+		BufferedOutputStream bufferedOutputStream = null;
+		
+		File file = serverSocket.receiveFile();
+		
+		System.out.println("Seleccione el destino del archivo: ");
+		
+		Thread.sleep(1500);
+		
+		JFileChooser jFileChooser = new JFileChooser(file);
+		jFileChooser.setMultiSelectionEnabled(false);
+		jFileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		
+		if(jFileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+			
+			System.out.println('\t'+jFileChooser.getSelectedFile().getPath()+'\n');
+			
+			bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(jFileChooser.getSelectedFile().getPath()));
+			bufferedOutputStream.write(file.getBytes());
+			
+			bufferedOutputStream.close();
+		}
+		
+		System.out.println("Su archivo se ha guardado con exito en "+jFileChooser.getSelectedFile().getPath());
+		
+		serverSocket.close();
+	}
+}
